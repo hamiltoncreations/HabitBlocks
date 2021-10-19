7767eefa022b3b6be86dc13822b4c81bfcb53142
Matthew Hamilton <matthew.edward.hamilton@gmail.com>
2021-10-18 20:24:04 -0400

    Initial Commit

---
+++ Habit Blocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit Blocks
+//
+//  Created by Matthew Hamilton on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habit Blocks/Habit_BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_BlocksApp.swift
+//  Habit Blocks
+//
+//  Created by Matthew Hamilton on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
